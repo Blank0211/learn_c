0d7737231989c5b05bfb7f4a9d31f21305194eab
Blank0211 <wkgchoh@gmail.com>
2023-11-11 12:20:01 +0530

    Chapter 8, repdigit.c (done)

--- waterfall.c
+++ waterfall.c
@@ -1,7 +1,11 @@
 // clang % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2
 #include <stdio.h>
-#define N 5
+#include <stdint.h>
+#define bool int8_t
+#define true 1
+#define false 0
 
+#define N 5
 void revnum(void) {
     // Reverses a series of numbers
     printf("Enter %d numbers: ", N);
@@ -12,8 +16,24 @@ void revnum(void) {
     for (int i = N-1; i >= 0; i--) printf("%d ", ns[i]);
 }
 
+void repdig(void) {
+    // Checks numbers for repeated digits
+    printf("Enter a number: ");
+    int n; scanf("%d", &n);
+
+    bool dig_seen[10] = {false};
+    while (n > 0) {
+        if (dig_seen[n%10]) break;
+
+        dig_seen[n%10] = true;
+        n /= 10;
+    }
+
+    printf("%s\n", n > 0 ? "Repeated digit" : "No repeated digit");
+}
+
 int main(void) {
-    revnum();
+    repdig();
     return 0;
 }
 
