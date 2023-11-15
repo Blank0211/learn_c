e0d4acb775733122af5a8daa6d581f0f3719665f
Blank0211 <wkgchoh@gmail.com>
2023-11-15 23:05:42 +0530

    Separate file for interest.c

---
+++ interest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define SIZE_a ((int) (sizeof(a) / sizeof(a[0])))
+
+void interest_c(void) {
+    printf("Enter interest rate: ");
+    int rate; scanf("%d", &rate);
+    printf("Enter number of years: ");
+    int yrs; scanf("%d", &yrs);
+
+    // Table headers
+    float a[5];
+    for (int i = 0; i < SIZE_a; i++) {
+        printf("%7d%%", rate + i);
+        a[i] = 100.00f; // init all values to 100.00f
+    }
+    printf("\n");
+
+    // Table contents
+    for (int i = 1; i <= yrs; i++) {
+        printf("   ");
+        // Int rates
+        for (int j = 0; j < SIZE_a; j++) {
+            int rt = rate + j;
+            // Display and update array
+            printf("%7.2f ", a[j] *= (rt / 100.0f + 1.0f));
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main(void) {
+    interest_c();
+    return 0;
+}
+
