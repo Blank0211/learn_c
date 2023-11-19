37c4fc6e8239bcae4c84a538744903c1a9444c67
Blank0211 <wkgchoh@gmail.com>
2023-11-19 21:58:24 +0530

    Chapter 8, Exercise 5: Fibionacci nums

--- waterfall.c
+++ waterfall.c
@@ -1,12 +1,21 @@
 // clang % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2 -g
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
-#include <stdlib.h>
-#define bool _Bool
 
+#define bool _Bool
+#define true 1
+#define false 0
+
+void fib(void) {
+    uint32_t fibs[40] = {0, 1};
+    for (int i = 2; i < 40; i++)
+        fibs[i] = fibs[i-1] + fibs[i-2];
+
+    for (int i = 0; i < 40; i++) printf("%d, ", fibs[i]);
+}
 
 int main(void) {
+    fib();
     return 0;
 }
 
