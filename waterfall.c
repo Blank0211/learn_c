243135bd3b6db87301c3b6d9f4d5acfdc6d0eab9
Blank0211 <wkgchoh@gmail.com>
2023-11-28 21:54:27 +0530

    Ch-8, Prj-3
    Add loop to previous project

--- waterfall.c
+++ waterfall.c
@@ -10,21 +10,25 @@
 void repdig(void) {
     // Checks a number for repeated digits and prints a table
     // showing how many time each digit appears in number.
-    printf("Enter a number: ");
-    int n; scanf("%d", &n);
+    while (true) {
+        printf("Enter a number: ");
+        int n; scanf("%d", &n);
+        if (n <= 0) return; // Terminate program if input <= 0.
 
-    // Check the number
-    int8_t dig_seen[DIGS] = {0};
-    while (n > 0) {
-        dig_seen[n%10]++;
-        n /= 10;
+        // Check the number
+        int8_t dig_seen[DIGS] = {0};
+        while (n > 0) {
+            dig_seen[n%10]++;
+            n /= 10;
+        }
+
+        // Print table
+        printf("Digit:        0  1  2  3  4  5  6  7  8  9\n");
+        printf("Occurrences:  ");
+        for (int i = 0; i < DIGS; i++)
+            printf("%d  ", dig_seen[i]);
+        printf("\n");
     }
-
-    // Print table
-    printf("Digit:        0  1  2  3  4  5  6  7  8  9\n");
-    printf("Occurrences:  ");
-    for (int i = 0; i < DIGS; i++)
-        printf("%d  ", dig_seen[i]);
 }
 
 int main(void) {
