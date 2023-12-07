27392e1f29cd41405f9a9545f6747ccb8323a7c1
Blank0211 <wkgchoh@gmail.com>
2023-12-07 20:53:22 +0530

    Move Prj-6 to separate file.

---
+++ chapter_8/biff.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MSG_LEN 80
+void biff(void) {
+    printf("Enter message: ");
+    char msg[MSG_LEN] = {0};
+    // Get & store message (until newline)
+    for (int i = 0; i < MSG_LEN; i++) {
+        msg[i] = getchar();
+        if (msg[i] == '\n') break;
+    }
+    
+    // Trasnlate to B1FF-speak
+    for (int c = 0; c < MSG_LEN; c++)
+        msg[c] = toupper(msg[c]); // Make all chars to uppercase
+    // Substitue digits for certain letters 
+    for (int c = 0; c < MSG_LEN; c++) {
+        switch (msg[c]) {
+            case 'A':
+                msg[c] = '4'; break;
+            case 'B':
+                msg[c] = '8'; break;
+            case 'E':
+                msg[c] = '3'; break;
+            case 'I':
+                msg[c] = '1'; break;
+            case 'O':
+                msg[c] = '0'; break;
+            case 'S':
+                msg[c] = '5'; break;
+            default:
+                break;
+        }
+    }
+
+    // Display message
+    printf("In B1FF-speak: ");
+    for (int i = 0; i < MSG_LEN; i++) {
+        if (msg[i] == '\n') break;
+        printf("%c", msg[i]);
+    } printf("!!!!!!!!!\n"); // Append exclamation marks
+}
+
+int main(void) {
+    biff();
+    return 0;
+}
+
