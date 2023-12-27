72b4f47c82ae6a2edd0087ad129abdb45d88e4df
Blank0211 <wkgchoh@gmail.com>
2023-12-27 22:22:14 +0530

    Ch-8, Prj-11

---
+++ chapter_08/prj11.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(void) {
+    printf("Enter a phone number: ");
+    char buf[16];
+    fgets(buf, sizeof(buf), stdin);
+
+    printf("In numeric form: ");
+    char c;
+    for (uint8_t i = 0; i < sizeof(buf); i++) {
+        c = buf[i];
+        switch (c) {
+            case 'A': case 'B': case 'C':
+                putchar('2'); break;
+            case 'D': case 'E': case 'F':
+                putchar('3'); break;
+            case 'G': case 'H': case 'I':
+                putchar('4'); break;
+            case 'J': case 'K': case 'L':
+                putchar('5'); break;
+            case 'M': case 'N': case 'O':
+                putchar('6'); break;
+            case 'P': case 'R': case 'S':
+                putchar('7'); break;
+            case 'T': case 'U': case 'V':
+                putchar('8'); break;
+            case 'W': case 'X': case 'Y':
+                putchar('9'); break;
+            default:
+                putchar(c); break;
+        }
+    }
+    
+    return 0;
+}
