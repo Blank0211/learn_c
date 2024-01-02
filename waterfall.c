1c8e1b7e0d4e1c7cbdb036e14c668163c10505e9
Blank0211 <wkgchoh@gmail.com>
2024-01-02 12:59:01 +0530

    Ch-8, Prj-13

--- waterfall.c
+++ waterfall.c
@@ -8,8 +8,32 @@
 #define true 1
 #define false 0
 
-int main(void) {
+void fl_name(void) {
+    // Data
+    char initial, c;
+    char l_name[21] = {0}; // Initialize all chars to NUL
 
+    // Input
+    printf("Enter a first and last name: ");
+    while ((initial = getchar()) == ' ') ; // Get first char of FN
+    while ((c = getchar()) != ' ') ; // Eat rest of chars in FN
+
+    int i = 0;
+    while (i < 20) {
+        c = getchar();
+        if (c == ' ') continue; // Skip spaces before, in-between & after LN
+        if (c == '\n') break;
+
+        l_name[i] = c;
+        i++;
+    }
+
+    // Output
+    printf("%s, %c.\n", l_name, initial);
+}
+
+int main(void) {
+    fl_name();
     return 0;
 }
 
