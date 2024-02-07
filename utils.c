045025f0062912932919e33c648f7e54db43f7ea
Blank0211 <wkgchoh@gmail.com>
2024-02-07 17:58:55 +0530

    Minor change: to_12h > to_h12

--- utils.c
+++ utils.c
@@ -10,7 +10,7 @@ char *ampm(int hour_24) {
     return "P.M.";
 }
 
-int to_12h(int hour_24) {
+int to_h12(int hour_24) {
     if (hour_24 == 0) return 12;
     if (hour_24 <= 12) return hour_24;
     return hour_24 % 12;
--- utils.h
+++ utils.h
@@ -1,6 +1,6 @@
 #include <inttypes.h>
 
-// Returns absolute val
+// Returns absolute value
 int abs(int n);
 
 // Returns "A.M." or "P.M." depending on hour
@@ -9,6 +9,6 @@ char *ampm(int hour_24);
 // Converts 24-hour time to 12-hour
 int to_12h(int hour_24);
 
-// Prints err_num and terminates program
+// Prints "\nERROR_[err_num]" and exits(0) program
 void err_exit(uint8_t err_num);
 
