c997fec8a8c061ea122fb8af0c313b116da67b66
Blank0211 <wkgchoh@gmail.com>
2024-02-07 17:53:07 +0530

    Add err_exit function

--- utils.c
+++ utils.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h> // Defines: exit
+#include <inttypes.h>
 #include "utils.h"
 
 int abs(int n) { return n < 0 ? -n : n; }
@@ -11,5 +14,10 @@ int to_12h(int hour_24) {
     if (hour_24 == 0) return 12;
     if (hour_24 <= 12) return hour_24;
     return hour_24 % 12;
+}
+
+void err_exit(uint8_t err_num) {
+    printf("\nERROR_%" PRIu8 "\n", err_num);
+    exit(0);
 }
 
--- utils.h
+++ utils.h
@@ -1,9 +1,14 @@
+#include <inttypes.h>
 
+// Returns absolute val
 int abs(int n);
 
+// Returns "A.M." or "P.M." depending on hour
 char *ampm(int hour_24);
 
+// Converts 24-hour time to 12-hour
 int to_12h(int hour_24);
 
-
+// Prints err_num and terminates program
+void err_exit(uint8_t err_num);
 
