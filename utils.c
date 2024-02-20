75a27989e7f85b51bc5e1b7866b91cc4b0e31ecb
Blank0211 <wkgchoh@gmail.com>
2024-02-20 15:57:18 +0530

    Change get_d32:
        Return value: void > int8_t
        Use getchar() instead of fgets()
        Add check if strtol does not convert str

--- utils.c
+++ utils.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h> // Defines: exit
+#include <stdlib.h> // Defines: exit, strtol
 #include <inttypes.h>
+#include <errno.h> // Defines: errno, ERANGE
 #include "utils.h"
+
+#define BUF_SIZE 32 // Used in get_d32;
 
 int abs(int n) { return n < 0 ? -n : n; }
 
@@ -20,4 +23,44 @@ void err_exit(uint8_t err_num) {
     printf("\nERROR_%" PRIu8 "\n", err_num);
     exit(0);
 }
+
+// Returns: 0 on success. 1 if strtol fails. 2 if input out of range.
+int8_t get_d32(int32_t *n) {
+    long tmp = 0;
+    *n = 0;
+
+    // === [[ INPUT ]] ===
+    printf("Enter a number: ");
+    char buf[BUF_SIZE] = {0};
+    uint8_t i = 0; // buf index
+    char c = 0;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        if (i < BUF_SIZE) buf[i++] = c;
+    }
+
+    // === [[ PROCESS INPUT ]] ===
+    // errno can be set to any non-zero value by a library function call
+    // regardless of whether there was an error, so it needs to be cleared
+    // in order to check the error set by strtol
+    errno = 0;
+    char *end;
+    tmp = strtol(buf, &end, 10);
+    if (end == buf) {
+        printf("\n --> strtol could not convert input\n");
+        return 1;
+    }
+    if (errno == ERANGE) {
+        printf("\n --> Range error occured.\n");
+        return 2;
+    }
+    
+    // === [[ SET n ]] ===
+    // Execution should only reach here if previous steps are successful.
+    // Set value of n.
+    if      (tmp > INT32_MAX) *n = INT32_MAX;
+    else if (tmp < INT32_MIN) *n = INT32_MIN;
+    else                      *n = (int32_t) tmp;
+    return 0;
+}
+
 
--- utils.h
+++ utils.h
@@ -12,3 +12,9 @@ int to_h12(int hour_24);
 // Prints "\nERROR_[err_num]" and exits(0) program
 void err_exit(uint8_t err_num);
 
+// Gets integer input & assigns it to n on success.
+// Sets n to 0, on failure.
+// Sets n to INT32_MAX or INT32_MIN, if value is out of range.
+// Returns 0 on success, non-zero on failure.
+int8_t get_d32(int32_t *n);
+
